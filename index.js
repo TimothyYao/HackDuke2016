530d23305305ba5fe0f702448a5b5d143b559043
timothyyao <timmyyao555@gmail.com>
2016-11-19 23:00:25 -0500

    deleted comments

--- index.js
+++ index.js
@@ -49,26 +49,6 @@ app.post('/webhook/', function (req, res) {
                 sendServiceOptions(sender);
                 continue
             }
-
-            // if (text === 'response') {
-            //   message = {
-            //     "text":"Pick a color:",
-            //     "quick_replies":[
-            //       {
-            //         "content_type":"text",
-            //         "title":"Red",
-            //         "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
-            //       },
-            //       {
-            //         "content_type":"text",
-            //         "title":"Green",
-            //         "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
-            //       }
-            //     ]
-            //   }
-            //   sendResponse(sender, message)
-            //   continue
-            // }
             if (text === 'help') {
                 sendTextMessage(sender, "Sorry, the help page has not yet been updated :C")
                 continue
