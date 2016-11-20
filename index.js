052ff8211c2b8e91743e4f3351f794dd065aad37
timothyyao <timmyyao555@gmail.com>
2016-11-19 20:05:36 -0500

    quick response

--- index.js
+++ index.js
@@ -40,25 +40,25 @@ app.post('/webhook/', function (req, res) {
         let sender = event.sender.id
         if (event.message && event.message.text) {
             let text = event.message.text
-            // if (text === 'test quick response') {
-            //   message = {
-            //     "text":"Pick a color:",
-            //     "quick_replies":[
-            //       {
-            //         "content_type":"text",
-            //         "title":"Red",
-            //         "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
-            //       },
-            //       {
-            //         "content_type":"text",
-            //         "title":"Green",
-            //         "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
-            //       }
-            //     ]
-            //   }
-            //   sendResponse(sender, message)
-            //   continue
-            // }
+            if (text === 'test quick response') {
+              message = {
+                "text":"Pick a color:",
+                "quick_replies":[
+                  {
+                    "content_type":"text",
+                    "title":"Red",
+                    "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
+                  },
+                  {
+                    "content_type":"text",
+                    "title":"Green",
+                    "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
+                  }
+                ]
+              }
+              sendResponse(sender, message)
+              continue
+            }
             if (text === 'help') {
               sendTextMessage(sender, "Sorry, the help page has not yet been updated :C")
               continue
