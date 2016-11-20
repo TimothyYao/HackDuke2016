aa6bd804d89750940f63b671882adb650fde937c
2changs <diana.chang05@gmail.com>
2016-11-19 20:52:17 -0500

    attachment check

--- index.js
+++ index.js
@@ -84,6 +84,9 @@ app.post('/webhook/', function (req, res) {
 
             sendTextMessage(sender, "Could not understand \"" + text.substring(0, 320) + "\".\nTry typing \"help\" for more information!")
         }
+        if (event.message && event.message.attachment) {
+            console.log(event.message.attachment)
+        }
     }
     res.sendStatus(200)
 })
