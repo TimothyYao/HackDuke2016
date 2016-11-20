41bb369d0f01bb8456c49f66dac31fa0203bb228
timothyyao <timmyyao555@gmail.com>
2016-11-19 20:07:30 -0500

    trying to fix response

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.post('/webhook/', function (req, res) {
         let sender = event.sender.id
         if (event.message && event.message.text) {
             let text = event.message.text
-            if (text === 'test quick response') {
+            if (text === 'response') {
               message = {
                 "text":"Pick a color:",
                 "quick_replies":[
