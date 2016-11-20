3e2f7194127de4f6e3d64e4428a6f567acb979b7
lnterest <erikhsong@gmail.com>
2016-11-19 21:19:56 -0500

    asdf

--- index.js
+++ index.js
@@ -125,6 +125,10 @@ function sendServiceOptions(sender) {
                         "type": "postback",
                         "title": "Health",
                         "payload": "Payload for first element in a generic bubble",
+                    }, {
+                        "type": "postback",
+                        "title": "Health",
+                        "payload": "Payload for first element in a generic bubble",
                     }],
                 }, {
                     "title": "Second card",
