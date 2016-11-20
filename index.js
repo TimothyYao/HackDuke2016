85848ec4c5c336bd0415f83fa57f7409cffb62b9
Joshua Kim <jk8ry@virginia.edu>
2016-11-19 19:17:14 -0500

    formatting

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ app.post('/webhook/', function (req, res) {
                 continue
             }
 
-            sendTextMessage(sender, "Could not understand \"" + text.substring(0, 320) + "\"\nType help for more information!")
+            sendTextMessage(sender, "Could not understand \"" + text.substring(0, 320) + "\".\nTry typing \"help\" for more information!")
         }
     }
     res.sendStatus(200)
