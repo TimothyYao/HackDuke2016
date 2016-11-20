5eba6b3a0f2a18349ff5fe6aa98d247f76b4eb79
timothyyao <timmyyao555@gmail.com>
2016-11-20 09:18:48 -0500

    merging

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.post('/createEvent', function(req, res) {
 
         console.log("The file was saved!");
     });
-    res.sendFile('index.html')
+    res.sendFile(__dirname + '/webapp/eventCreated.html')
 
 })
 
