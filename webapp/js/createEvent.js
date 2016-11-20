09ad2e9aa8bb3c47807399024a34d79bf128fdc0
2changs <diana.chang05@gmail.com>
2016-11-20 12:46:23 -0500

    removed post function

--- webapp/js/createEvent.js
+++ webapp/js/createEvent.js
@@ -101,21 +101,23 @@
             }
         })
         .on('success.form.bv', function(e) {
-            $('#success_message').slideDown({ opacity: "show" }, "slow") // Do something ...
-                $('#contact_form').data('bootstrapValidator').resetForm();
+            // $('#success_message').slideDown({ opacity: "show" }, "slow") // Do something ...
+            //     $('#contact_form').data('bootstrapValidator').resetForm();
 
             // Prevent form submission
-            e.preventDefault();
+            //e.preventDefault();
 
             // Get the form instance
             var $form = $(e.target);
 
             // Get the BootstrapValidator instance
             var bv = $form.data('bootstrapValidator');
+            console.log($form.serialize());
+            console.log('asdf')
 
             // Use Ajax to submit form data
-            $.post($form.attr('action'), $form.serialize(), function(result) {
-                console.log(result);
-            }, 'json');
+            // $.post($form.attr('action'), $form.serialize(), function(result) {
+            //     console.log(result);
+            // }, 'json');
         });
 });
