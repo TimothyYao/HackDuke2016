724136f61b39436be7970cc3d1824f7df3ca5883
2changs <diana.chang05@gmail.com>
2016-11-19 19:06:50 -0500

    committed

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.post('/webhook/', function (req, res) {
             let text = event.message.text
             let attached = event.message.attachment
             if (text === 'help') {
-              sendTextMessage(sender, "Sorry, the help page has not been updated yet :c")
+              sendTextMessage(sender, "Sorry, the help page has not yet been updated :C")
               continue
             }
             if (text === 'Generic') {
@@ -64,23 +64,30 @@ app.post('/webhook/', function (req, res) {
     res.sendStatus(200)
 })
 
+function sendResponse(sender, data, callback) {
+  let messageData = data
+  request({
+      url: 'https://graph.facebook.com/v2.6/me/messages',
+      qs: {access_token:token},
+      method: 'POST',
+      json: {
+          recipient: {id:sender},
+          message: messageData,
+      }
+  }, function(error, response, body) {
+      if (error) {
+          console.log('Error sending messages: ', error)
+      } else if (response.body.error) {
+          console.log('Error: ', response.body.error)
+      }
+  })
+  if (typeof callback === "function") {
+       callback();
+   }
+}
+
 function sendTextMessage(sender, text) {
-    let messageData = { text:text }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
+    sendResponse(sender, { text:text });
 }
 
 function sendGenericMessage(sender) {
