9a2577305645159584b85736c7f12653ceae4b68
timothyyao <timmyyao555@gmail.com>
2016-11-20 09:35:11 -0500

    testing callback

--- index.js
+++ index.js
@@ -148,7 +148,7 @@ app.post('/webhook/', function (req, res) {
                 continue
             }
             if (lowerText === 'help') {
-                sendTextMessage(sender, 'Type \"start\" to begin your search!')
+                sendTextMessage(sender, 'Type \"start\" to begin your search!', sendTextMessage('1', sendTextMessage('2')))
                 continue
             }
             if (lowerText === 'location') {
