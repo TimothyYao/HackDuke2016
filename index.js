2864412c492ff7fcd10a7251b6901cc138419181
lnterest <erikhsong@gmail.com>
2016-11-19 19:16:43 -0500

    testing

--- index.js
+++ index.js
@@ -157,7 +157,6 @@ function getFirstName(sender) {
         } else if (response.body.error) {
             console.log('Error: ', response.body.error)
         }
-        console.log(response.statusCode)
+        return response.statusText;
     })
-    return "asdf"
 }
