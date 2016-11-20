ce40e1f30d725bed534af321c7844af050e6b859
timothyyao <timmyyao555@gmail.com>
2016-11-19 20:16:18 -0500

    console prints sender

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ app.post('/webhook/', function (req, res) {
         let sender = event.sender.id
         if (event.message && event.message.text) {
             let text = event.message.text
+            console.log(sender)
             // if (text === 'response') {
             //   message = {
             //     "text":"Pick a color:",
